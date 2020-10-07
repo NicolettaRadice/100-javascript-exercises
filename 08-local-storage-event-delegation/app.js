9dfc1da637c0faedae5c74adad7c2254f7a85bb2
Nicoletta <54234635+NicolettaRadice@users.noreply.github.com>
2020-10-07 15:32:15 +0200

    day 08

---
+++ 08-local-storage-event-delegation/app.js
@@ -0,0 +1,50 @@
+// local stoarege -> data stored in the browser will persist even after the browser window has been closed.
+// event delegation -> go to the parent and tell children to do something
+
+const addItems = document.querySelector('.add-items');
+const itemsList = document.querySelector('.plates');
+// const check_all = document.querySelector('#check-all');
+// const uncheck_all = document.querySelector('#uncheck-all');
+const items = JSON.parse(localStorage.getItem('items')) || [];
+
+function addItem(e) {
+    e.preventDefault();
+    const text = (this.querySelector('[name=item')).value;
+    const item = {
+        text,
+        done: false
+    }
+    items.push(item);
+    populateList(items, itemsList);
+    localStorage.setItem('items', JSON.stringify(items));
+    this.reset();
+}
+
+function populateList(plates = [], platesList) {
+    platesList.innerHTML = plates.map((plate, i) => {
+      return `
+        <li>
+          <input type="checkbox" class="check" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''} />
+          <label for="item${i}">${plate.text}</label>
+        </li>
+      `;
+    }).join('');
+}
+
+function toggleDone(e) {
+    if (!e.target.matches('input')) return; // skip this unless it's an input
+    const el = e.target;
+    const index = el.dataset.index;
+    
+    items[index].done = !items[index].done;
+
+    localStorage.setItem('items', JSON.stringify(items));
+    populateList(items, itemsList);
+}
+
+addItems.addEventListener('submit', addItem);
+// check_all.addEventListener('click', checkAll);
+// uncheck_all.addEventListener('click', uncheckAll);
+itemsList.addEventListener('click', toggleDone);
+populateList(items, itemsList);
+

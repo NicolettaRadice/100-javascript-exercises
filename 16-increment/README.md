19ce62ec044fc65171d0c1da51db08b43867b713
Nicoletta <54234635+NicolettaRadice@users.noreply.github.com>
2020-11-04 15:15:21 +0100

    day 16

---
+++ 16-increment/README.md
@@ -0,0 +1,4 @@
+# Increment
+
+Fot today I focused on incrememnt. After reading the documentation (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment) I made a small interface.
+
---
+++ 16-increment/app.js
@@ -0,0 +1,13 @@
+var button = document.getElementById("button");
+const span = document.getElementById("number");
+
+count = 0;
+
+button.onclick = function() {
+	count += 1;
+	number.innerHTML = " " + count;
+};
+
+
+
+
